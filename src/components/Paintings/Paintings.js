860ae5201056693f8d0161045d9850c17bd6247c
GuyAfik <guyafik11@gmail.com>
2024-02-05 23:59:37 +0200

    improve paintings UX

--- src/components/Paintings/Paintings.js
+++ src/components/Paintings/Paintings.js
@@ -15,6 +15,7 @@ const Paintings = () => {
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [viewMode, setViewMode] = useState(false);
 
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -28,12 +29,41 @@ const Paintings = () => {
 
   const handleThumbnailClick = (index) => {
     setCurrentIndex(index);
+    setViewMode(true);
+  };
+
+  const handleCloseViewMode = () => {
+    setViewMode(false);
   };
 
   return (
     <div className="paintings-page">
+      {viewMode && (
+        <div className="better-view-mode">
+          <button className="exit-button" onClick={handleCloseViewMode}>
+            X
+          </button>
+          <img
+            src={images[currentIndex]}
+            alt={`Image ${currentIndex + 1}`}
+            className="better-view-image"
+          />
+          <button className="prev-button" onClick={handlePrev}>
+            &lt; Prev
+          </button>
+          <button className="next-button" onClick={handleNext}>
+            Next &gt;
+          </button>
+        </div>
+      )}
+
       <h1>Paintings</h1>
-      <p>Painting has been a main technique in my art for many years. These days my interest in installation has increased, but I often return to painting, insisting not to neglect my painting skills.</p>
+      <p>
+        Painting has been a main technique in my art for many years. These days
+        my interest in installation has increased, but I often return to
+        painting, insisting not to neglect my painting skills.
+      </p>
+
       <div className="paintings-gallery-container">
         <div className="paintings-preview">
           {images.map((image, index) => (
@@ -41,19 +71,20 @@ const Paintings = () => {
               key={index}
               src={image}
               alt={`Preview ${index + 1}`}
-              className={`thumbnail ${index === currentIndex ? "active" : ""}`}
+              className={`thumbnail ${
+                index === currentIndex ? "active" : ""
+              }`}
               onClick={() => handleThumbnailClick(index)}
             />
           ))}
         </div>
         <div className="paintings-main">
-          <button onClick={handlePrev}>&lt; Prev</button>
           <img
             src={images[currentIndex]}
             alt={`Image ${currentIndex + 1}`}
             className="paintings-gallery-image"
+            onClick={() => setViewMode(true)}
           />
-          <button onClick={handleNext}>Next &gt;</button>
         </div>
       </div>
     </div>
