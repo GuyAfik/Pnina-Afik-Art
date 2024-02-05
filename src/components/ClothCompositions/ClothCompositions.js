39063375d6ef248c03450b21064f6efe4b35ac9d
GuyAfik <guyafik11@gmail.com>
2024-02-06 00:04:19 +0200

    adjust cloth compositions

--- src/components/ClothCompositions/ClothCompositions.js
+++ src/components/ClothCompositions/ClothCompositions.js
@@ -1,5 +1,4 @@
 // ClothCompositions.js
-
 import React, { useState } from "react";
 import "./ClothCompositions.css";
 
@@ -18,6 +17,7 @@ const ClothCompositions = () => {
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [viewMode, setViewMode] = useState(false);
 
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
@@ -31,15 +31,41 @@ const ClothCompositions = () => {
 
   const handleThumbnailClick = (index) => {
     setCurrentIndex(index);
+    setViewMode(true);
+  };
+
+  const handleCloseViewMode = () => {
+    setViewMode(false);
   };
 
   return (
     <div className="cloth-compositions-page">
+      {viewMode && (
+        <div className="better-view-mode">
+          <button className="exit-button" onClick={handleCloseViewMode}>
+            X
+          </button>
+          <img
+            src={images[currentIndex]}
+            alt={`Image ${currentIndex + 1}`}
+            className="better-view-image"
+            onClick={handleCloseViewMode} // Close view mode when clicking on the image
+          />
+          <button className="prev-button" onClick={handlePrev}>
+            &lt; Prev
+          </button>
+          <button className="next-button" onClick={handleNext}>
+            Next &gt;
+          </button>
+        </div>
+      )}
+
       <h1>Cloth compositions</h1>
       <p>
         I choose clothes from my wardrobe; I cut and create new compositions
         based on clothes fir my own wardrobe. Combining ropes, canvas, paint.
       </p>
+
       <div className="cloth-compositions-gallery-container">
         <div className="cloth-compositions-preview">
           {images.map((image, index) => (
@@ -55,13 +81,12 @@ const ClothCompositions = () => {
           ))}
         </div>
         <div className="cloth-compositions-main">
-          <button onClick={handlePrev}>&lt; Prev</button>
           <img
             src={images[currentIndex]}
             alt={`Image ${currentIndex + 1}`}
             className="cloth-compositions-gallery-image"
+            onClick={() => setViewMode(true)}
           />
-          <button onClick={handleNext}>Next &gt;</button>
         </div>
       </div>
     </div>
