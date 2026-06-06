b8c33ad926d9c9b019322558a0bb92ae553613d1
Guy Afik <gafik@paloaltonetworks.com>
2026-06-06 12:15:34 +0300

    about card update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -2,6 +2,13 @@ import Card from "react-bootstrap/Card";
 
 
 function AboutCard() {
+  const sectionStyle = {
+    display: "block",
+    fontWeight: "bold",
+    marginTop: "1em",
+    marginBottom: "0.3em",
+  };
+
   return (
     <Card className="quote-card-view">
       <Card.Body>
@@ -14,93 +21,84 @@ function AboutCard() {
             A multi-disciplinary artist
             <br />
             Art graduate of Haifa University
-            <h3>
-              <br />
-              Selected Solo Exhibition
-            </h3>
+            <span style={sectionStyle}>Selected Solo Exhibitions</span>
             2026
             <br />
             <br />
-            "Holding the Fragments"- Palazzo Mora, Venice (Part of Personal Structures Project running alongside the Art Biennale)
+            "Holding The Fragments"- Palazzo Mora, Venice (Part of Personal Structures Project running alongside the Art Biennale)
             <br />
             <br />
             2024
             <br />
             <br />
-            HOW TO VOWELIZE A VOWEL kubia (cube) 6 Tel Aviv
-            <br />
-            "Angel on Shore"- Cube 6 Gallery, Tel Aviv
+            "Angel On Shore"- Cube 6 Gallery, Tel Aviv
             <br />
             <br />
             2023-24
             <br />
             <br />
-            "WHERE DID THE MORNING DISAPPEAR? Gan Shmuel Gallery.
+            "Where Did The Morning Disappear?"- Gan Shmuel Gallery
             <br />
             <br />
             2021
             <br />
             <br />
-            "SHADOW OF DOUBT" Yavne Gallery.
+            "Shadow Of Doubt"- Yavne Gallery
             <br />
-            "HIDING PLACE" N.D. Gallery Ramat Gan.
+            "Hiding Place"- N.D. Gallery, Ramat Gan
             <br />
-            "TWILLIGHT", Gallery on the Hill- Maitar.
+            "Twilight"- Gallery On The Hill, Maitar
             <br />
             <br />
             2018
             <br />
             <br />
-            "SUPERPOSITION" TOVA OSMANN GALLEY- TEL AVIV
+            "Superposition"- Tova Osmann Gallery, Tel Aviv
             <br />
-            "ONE BY ONE" EIN HOD GALLERY- EIN HOD
-            <h3>
-              <br />
-              Selected Group Exhibitions
-            </h3>
-            <br />
+            "One By One"- Ein Hod Gallery, Ein Hod
+            <span style={sectionStyle}>Selected Group Exhibitions</span>
             2026
             <br />
             <br />
-            "Conversation with a Tree"- outdoor exhibition
+            "Conversation With A Tree"- Outdoor Exhibition
             <br />
             <br />
             2025
             <br />
             <br />
-            "Oh, I am Dressed up"- outdoor exhibition
+            "Oh, I Am Dressed Up"- Outdoor Exhibition
             <br />
             <br />
             2023
             <br />
             <br />
-            "How to Vowelize"- Cube 6 Gallery Tel Aviv
+            "How To Vowelize"- Cube 6 Gallery, Tel Aviv
             <br />
-            "In Real time"- Cube 6 Gallery Tel Aviv
+            "In Real Time"- Cube 6 Gallery, Tel Aviv
             <br />
-            "HERE I AM" Starta Arta Gallery New York.
+            "Here I Am"- Starta Arta Gallery, New York
             <br />
             <br />
             2022
             <br />
             <br />
-            "FALSE AWAKENING" Givat Haviva Gallery.
+            "False Awakening"- Givat Haviva Gallery
             <br />
             <br />
             2021
             <br />
             <br />
-            "ROOTS" Zurala group exhibition, Gallery on the Cliff, Netanya.
+            "Roots"- Zurala Group Exhibition, Gallery On The Cliff, Netanya
             <br />
             <br />
             2020
             <br />
             <br />
-            THE NEW ATTRIBUTES. Rishon Lezion Artists House.
+            "The New Attributes"- Rishon Lezion Artists House
             <br />
-            "CORONA TIME"- Gan Shmuel Gallery.
+            "Corona Time"- Gan Shmuel Gallery
             <br />
-            "RORSHACH IS ALIVE AND DEAD IN TEL AVIV"- ONLINE EXHIBITION (Covid days)
+            "Rorschach Is Alive And Dead In Tel Aviv"- Online Exhibition (Covid Days)
             <br />
             https://www.facebook.com/hanitaelizur/photos/rpp.103224641337792/114499426876980/?type=3&theater
             <br />
@@ -108,39 +106,26 @@ function AboutCard() {
             2019
             <br />
             <br />
-            "WOMEN CREATORS"- Csstel Museum- Ma'ale Edomim
+            "Women Creators"- Csstel Museum, Ma'ale Edomim
             <br />
-            INTERNATIONAL ART BIENNALE- Museum of Modern Art Barcelona.
+            "International Art Biennale"- Museum Of Modern Art, Barcelona
             <br />
-            ART EXPO NEW YORK- a contemporary art show- NEW YORK U.S.A
-            ART SAN DIEGO—a contemporary art show-San Diego USA
-            SPECTRUM MIAMI- CONTEMPORARY INTERNATIONAL ART SHOW, MIAMI USA
-            <h3>
-              <br />
-              SELECTED PUBLICATIONS
-            </h3>
+            "Art Expo New York"- A Contemporary Art Show, New York, U.S.A
+            <br />
+            "Art San Diego"- A Contemporary Art Show, San Diego, USA
+            <br />
+            "Spectrum Miami"- Contemporary International Art Show, Miami, USA
+            <span style={sectionStyle}>Selected Publications</span>
             Texture Magazine
-            <br />
-            www.saatchiart.com
-            <br />
-            www.artdoxa.com
             <br />
             - Art International Contemporary Magazine 2019 (print magazine)
             <br />
-            - 44 Degrees Online Art Magazine - 31st Edition- "There Are no Sweet Wars" - https://issuu.com/44degrees/docs/44degrees-
+            - 44 Degrees Online Art Magazine - 31st Edition- "There Are No Sweet Wars"
             <br />
-            - Landscape Art Review Online Magazine- 10th edition- 24 page. Interview article Published mid-September 2019
-
-            https://issuu.com/land.previews/docs/pninaafikpreviewlandescapeartreview
-            <h3>
-              <br />
-              PRIZES
-            </h3>
-            2019- INTERNATIONAL VELASQUES ART PRIZE – Barcelona Spain
-            <h3>
-              <br />
-              MAIN WORKING POSITIONS
-            </h3>
+            - Landscape Art Review Online Magazine- 10th Edition- 24 page. Interview article published mid-September 2019
+            <span style={sectionStyle}>Prizes</span>
+            2019- International Velasquez Art Prize – Barcelona, Spain
+            <span style={sectionStyle}>Main Working Positions</span>
             <br />
             - Art teacher at Mevo'ot Yam, a professional marine studies high school (mainly for boys).
             <br />
@@ -150,7 +135,7 @@ function AboutCard() {
             <br />
             - Art director of Netanya Municipality
             <br />
-            - Had established "Zurala", a group of 5 women artists of 5 different disciplines:   painting, sculpture, photography, ceramic and multi discipline art.
+            - Had established "Zurala", a group of 5 women artists of 5 different disciplines: painting, sculpture, photography, ceramic and multi discipline art.
           </p>
            <div style={{ marginTop: "30px", textAlign: "center" }}>
               <h4 style={{ marginBottom: "15px", color: "black" }}>
