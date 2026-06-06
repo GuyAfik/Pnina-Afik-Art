8f03229bea7dfe65ce9fe7b63d2d0d45af2827ea
Guy Afik <gafik@paloaltonetworks.com>
2026-06-06 10:13:54 +0300

    smaller text

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -6,7 +6,7 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-        <p style={{ textAlign: "justify", color: "black", fontSize: "0.9em" }}>
+        <p style={{ textAlign: "justify", color: "black", fontSize: "0.75em" }}>
             Born, lives and works in Israel
             <br />
             Pnina.afik@gmail.com
